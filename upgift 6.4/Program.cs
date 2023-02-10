3cb287a447b03666f4521e3249485eb9c16fbed5
JesperWintberg <jesper.wintberg@gmail.com>
2023-02-10 11:04:48 +0100

    Add project files.

---
+++ upgift 6.4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            något();
+
+
+        }
+
+        static void något()
+        {
+            Console.WriteLine("Skriv in ditt bas tal");
+            string bas = Console.ReadLine();
+            Console.WriteLine("Skriv in ditt exponent tal");
+            string exponent = Console.ReadLine(); 
+
+            int tal1 = int.Parse(bas);
+            int tal2 = int.Parse(exponent);
+
+            int tal3 = 1;
+
+            for(int i = 0; i < tal2;i++)
+            {
+                tal3 = tal1 * tal3;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(tal3);
+        }        
+    }
+}
